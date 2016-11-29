d2d05ffbfc0127b85c6879b4012f70f66f647af3
MorePlastic <mathiasvasiliadis@gmail.com>
2016-11-29 19:42:52 +0100

    Travel Tales
    Arbejdet på Travel Tales (backlog nr. 16)

--- src/js/foo.js
+++ src/js/foo.js
@@ -1,3 +1,11 @@
+$(document).ready(function () {
+
+
+});
+
+
+
+
 $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
         var target = $(this.hash);
@@ -18,5 +26,36 @@ function myMap() {
         center: new google.maps.LatLng(51.5, -0.2), zoom: 10
     };
     var map = new google.maps.Map(mapCanvas, mapOptions);
+}
+
+
+//HERFRA OG NED ER OUTPUTTET NÅR MAN TRYKKER PÅ KNAPPERNE I TRAVEL TALES
+//Vi kan også skrive det her i HTML filen under hver knap i stedet
+
+//By 1
+function getTalesOutputTown1() {
+    $('#talesOutputBox').text("Her skal der stå text om by 1");
+}
+//By 2
+function getTalesOutputTown2() {
+    $('#talesOutputBox').text("Her skal der stå text om by 2");
+}
+
+//By 3
+function getTalesOutputTown3() {
+    $('#talesOutputBox').text("Her skal der stå text om by 3");
+}
+
+//By 4
+function getTalesOutputTown4() {
+    $('#talesOutputBox').text("Her skal der stå text om by 4");
+}
+
+function getTalesOutputTown5() {
+    $('#talesOutputBox').text("Her skal der stå text om by 5");
+}
+
+function getTalesOutputTown6() {
+    $('#talesOutputBox').text("Her skal der stå text om by 6");
 }
 
