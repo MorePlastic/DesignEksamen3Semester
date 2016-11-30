88870146bf895dd24cfd2faa7674d0db652c28a8
heri270 <heri270@hotmail.com>
2016-11-30 17:07:43 +0100

    Backlog 16 - virker ikke

--- src/js/foo.js
+++ src/js/foo.js
@@ -1,60 +1,59 @@
 $(document).ready(function () {
-
-
 });
 
+// $('a[href*="#"]:not([href="#"])').click(function() {
+//     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+//         var target = $(this.hash);
+//         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+//         if (target.length) {
+//             $('html, body').animate({
+//                 scrollTop: target.offset().top
+//             }, 1000);
+//             return false;
+//         }
+//     }
+// });
 
-
-
-$('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 1000);
-            return false;
-        }
-    }
-});
-
-
-function myMap() {
-    var mapCanvas = document.getElementById("map");
-    var mapOptions = {
-        center: new google.maps.LatLng(51.5, -0.2), zoom: 10
-    };
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-}
-
+// function myMap() {
+//     var mapCanvas = document.getElementById("map");
+//     var mapOptions = {
+//         center: new google.maps.LatLng(51.5, -0.2), zoom: 10
+//     };
+//     var map = new google.maps.Map(mapCanvas, mapOptions);
+// }
 
 //HERFRA OG NED ER OUTPUTTET NÅR MAN TRYKKER PÅ KNAPPERNE I TRAVEL TALES
 //Vi kan også skrive det her i HTML filen under hver knap i stedet
 
-//By 1
+//Vienna
 function getTalesOutputTown1() {
-    $('#talesOutputBox').text("Her skal der stå text om by 1");
+    $('#talesTitle').text("Vienna");
+    $('#descriptionVienna').text("Her er Vienna");
 }
-//By 2
+//London
 function getTalesOutputTown2() {
-    $('#talesOutputBox').text("Her skal der stå text om by 2");
+    $('#talesOutputBox').text("The day had been planned and it was finally time for our obligatory visit to the famous Camden Market." +
+        "We had already been several times so we knew very well what to expect and did not think we would be surprised," +
+        "but we would soon learn otherwise." +
+        "We started out walking down Camden High Street checking out the colourful shops and eventually ended up in the Camden Lock Market where we had a look around the many,"+
+        "many food stands in search of finding just the right plate of lunch."+
+        "In all honesty, food is one of the most important things for us when traveling and we enjoy trying a little bit of everything when we are exploring food markets."+
+        "Right next to the lock, we found an even cuter food market than we had previous times and we settled on a wrap filled with chicken, cheese, veggies, and – behold – green capsicum."+
+        "We were not too impressed. Green capsicum is always too sharp in its taste, we find, but not this one! It was instant love.");
 }
-
-//By 3
+//Dublin
 function getTalesOutputTown3() {
     $('#talesOutputBox').text("Her skal der stå text om by 3");
 }
-
-//By 4
+//Copenhagen
 function getTalesOutputTown4() {
     $('#talesOutputBox').text("Her skal der stå text om by 4");
 }
-
+//Stenløse
 function getTalesOutputTown5() {
     $('#talesOutputBox').text("Her skal der stå text om by 5");
 }
-
+//Tehran
 function getTalesOutputTown6() {
     $('#talesOutputBox').text("Her skal der stå text om by 6, jeg skriver bare en masse her for at se hvordan det her kommer til at se ud");
 }
