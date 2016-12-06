bfed6192860a5beb401087d508af55ae0111fddc
heri270 <heri270@hotmail.com>
2016-12-06 22:27:14 +0100

    Rettelser i skrift og karusel

--- src/js/foo.js
+++ src/js/foo.js
@@ -1,29 +1,6 @@
 $(document).ready(function () {
 });
 
-// $('a[href*="#"]:not([href="#"])').click(function() {
-//     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-//         var target = $(this.hash);
-//         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-//         if (target.length) {
-//             $('html, body').animate({
-//                 scrollTop: target.offset().top
-//             }, 1000);
-//             return false;
-//         }
-//     }
-// });
-
-// function myMap() {
-//     var mapCanvas = document.getElementById("map");
-//     var mapOptions = {
-//         center: new google.maps.LatLng(51.5, -0.2), zoom: 10
-//     };
-//     var map = new google.maps.Map(mapCanvas, mapOptions);
-// }
-
-//HERFRA OG NED ER OUTPUTTET NÅR MAN TRYKKER PÅ KNAPPERNE I TRAVEL TALES
-
 //Vienna
 function getTalesOutputTown1() {
     $('#talesTitle').text("Vienna");
